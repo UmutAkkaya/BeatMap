497e8bb243c7ce5db91cdaf83ff10b9ef6218239
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2018-05-08 21:56:48 -0400

    Added track model/route + add track function

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ const express = require('express');
 const router = new express.Router();
 const mongoose = require('mongoose');
 const eventRoutes = require('./routes/events');
+const trackRoutes = require('./routes/tracks');
 
 // configure the express server
 const CLIENT_ID = '8f0471703d644ad694d2f1532ebf8388';
@@ -85,5 +86,6 @@ router.get('/callback', (req, res) => {
 });
 
 router.post('/events', eventRoutes.createEvent);
+router.post('/tracks', trackRoutes.addTrack);
 
 module.exports = router;
